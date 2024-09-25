c9738c6129171967656a288ae90d54182d804a29
ashokonmi3 <ashokonmi3@gmail.com>
2024-09-25 11:55:11 +0530

    Add files via upload

---
+++ test_task_fail.py
@@ -0,0 +1,20 @@
+"""Use the Task type to show test failures."""
+from tasks import Task
+
+# Task element types : [summary: str, owner: str, done: bool, id: int]
+# Task = namedtuple("Task", ["summary", "owner", "done", "id"])
+# Task.__new__.__defaults__ = (None, None, False, None)
+
+
+def test_task_equality():
+    """Different tasks should not be equal."""
+    t1 = Task('sit there', 'brian')
+    t2 = Task('do something', 'okken')
+    assert t1 == t2
+
+
+def test_dict_equality():
+    """Different tasks compared as dicts should not be equal."""
+    t1_dict = Task('make sandwich', 'okken')._asdict()
+    t2_dict = Task('make sandwich', 'okkem')._asdict()
+    assert t1_dict == t2_dict
