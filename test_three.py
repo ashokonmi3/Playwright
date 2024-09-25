df61549c4758971eb310784971d3f0a209fe50bc
ashokonmi3 <ashokonmi3@gmail.com>
2024-09-25 09:29:12 +0530

    Add files via upload

---
+++ test_three.py
@@ -0,0 +1,21 @@
+"""Test the Task data type."""
+import pytest
+from collections import namedtuple
+
+Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
+Task.__new__.__defaults__ = (None, None, False, None)
+
+
+def test_defaults():
+    """Using no parameters should invoke defaults."""
+    t1 = Task()
+    t2 = Task(None, None, False, None)
+    assert t1 == t2
+
+@pytest.mark.smoke
+def test_member_access():
+    """Check .field functionality of namedtuple."""
+    t = Task('buy milk', 'brian')
+    assert t.summary == 'buy milk'
+    assert t.owner == 'brian'
+    assert (t.done, t.id) == (False, None)
